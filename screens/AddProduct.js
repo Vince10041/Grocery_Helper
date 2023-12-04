225cf4c1cc4ea0e21c8037b9991cf6269f634970
Vince Lam <vince10041@gmail.com>
2023-12-04 17:03:15 +0800

    Testing commit with new account

--- screens/AddProduct.js
+++ screens/AddProduct.js
@@ -1,11 +1,12 @@
 // --- Importing libraries ---
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-native';
 
 function AddProduct () {
     return (
         <SafeAreaView style={styles}>
             <Text>This is the addProduct</Text>
+            <Button>Click Me</Button>
         </SafeAreaView>
     );
 }
